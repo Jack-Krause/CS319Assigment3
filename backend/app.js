26ca3fa1b31e7fabebd29904adb1c665f206f2da
Jack Krause <jmkrause@iastate.edu>
2023-12-09 01:10:37 -0600

    app.js

---
+++ backend/app.js
@@ -0,0 +1,7 @@
+var express = require("express");
+var cors = require("cors");
+var app = express();
+var fs = require("fs");
+var bodyParser = require("body-parser");
+var axios = require("axios");
+const { MongoClient } = require("mongodbg");
