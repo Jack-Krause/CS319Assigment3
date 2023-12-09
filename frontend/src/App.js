ff3b439ce2aad41413c6e872aa15c89e1b1b7579
Jack Krause <jmkrause@iastate.edu>
2023-12-09 17:49:32 -0600

    imports fix frontned

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import DeleteView from "./components/DeleteView";
-import NavBar from "./components/DeleteView";
+import NavBar from "./components/NavBar";
 import PostView from "./components/PostView";
 import ReadView from "./components/ReadView";
 import StudentInfo from "./components/StudentInfo";
