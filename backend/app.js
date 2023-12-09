4345298e32a712d6d74d03539e2936a0dc30f5ec
Jack Krause <jmkrause@iastate.edu>
2023-12-09 01:52:56 -0600

    setup

--- backend/app.js
+++ backend/app.js
@@ -11,8 +11,28 @@ app.use(bodyParser.json());
 
 const port = "8081";
 const host = "localhost";
+const url = "mongodb://127.0.0.1:27017";
+const dbName = "reactdata";
+const client = new MongoClient(url);
+const db = client.db(dbName);
 
 app.listen(port, () => {
     console.log("App listening at http://%s:%s", host, port);
+});
+
+
+// GET request method
+app.get("/listProducts", async(req, res) => {
+    await client.connect();
+    console.log("Node connected successfully to MongoDB for GET");
+    const query = {};
+    const results = await db
+        .collection("fakestore_catalog")
+        .find(query)
+        .limit(100)
+        .toArray();
+    console.log(results);
+    res.status(200);
+    res.send(result);
 });
 
