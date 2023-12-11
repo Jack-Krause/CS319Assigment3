a5654a466436ec82a8c05cb4185f69071d169ac0
Jack Krause <jmkrause@iastate.edu>
2023-12-10 19:22:10 -0600

    fixes

--- frontend/src/components/ReadView.js
+++ frontend/src/components/ReadView.js
@@ -1,34 +1,32 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import { useSearchView } from "../context/SearchViewContext";
 
 const ReadView = () => {
   // context methods and values from SearchViewContext
-  const { 
-    searchId, 
-    setSearchId, 
-    fetchId, 
+  const {
+    searchId,
+    setSearchId,
+    fetchId,
     fetchAllProducts,
     setAllProducts,
     allProducts,
     setProductById,
-    productById
+    productById,
   } = useSearchView();
 
   // handle changing the context of what the user searched
   const handleInputChange = (event) => {
     setSearchId(event.target.value);
-    handleSearching();
   };
 
   const handleSearching = async () => {
-
     try {
-      if (searchId.trim() === '') {
+      if (searchId.trim() === "") {
         await fetchAllProducts();
         setProductById(null);
       } else {
         await fetchId(searchId);
-        setAllProducts([]);
+        // setAllProducts([]);
       }
     } catch (error) {
       console.error(error);
@@ -44,36 +42,38 @@ const ReadView = () => {
               type="text"
               value={searchId}
               onChange={handleInputChange}
-              placeHolder="Show All Products"
+              placeholder="Enter an ID"
             />
             <button onClick={handleSearching}>
-              {searchId.trim() === '' 
-              ? 'Show All Products'
-              : isNaN(searchId) || !Number.isInteger(parseFloat(searchId))
-              ? 'Enter an integer for ID'
-              : `Show product ID: ${searchId}`}
-              </button>
+              {searchId.trim() === ""
+                ? "Show All Products"
+                : isNaN(searchId) || !Number.isInteger(parseFloat(searchId))
+                ? "Enter an integer for ID"
+                : `Show product ID: ${searchId}`}
+            </button>
             <div id="productContainer">
-              {searchId.trim() === '' ? (
+              {productById ? (
+                <div className="product-item">
+                  <h2>
+                    ID: {productById.id} {productById.title}
+                  </h2>
+                  <p>Price: {productById.price}</p>
+                  <p>Description: {productById.description}</p>
+                  <p>Category: {productById.category}</p>
+                  <p>Rating: {productById.rating}</p>
+                </div>
+              ) : (
                 allProducts.map((product, index) => (
                   <div className="product-item" key={index}>
-                    <h2>{product.title}</h2>
+                    <h2>
+                      ID: {product.id} - {product.title}
+                    </h2>
                     <p>Price: {product.price}</p>
                     <p>Description: {product.desciption}</p>
                     <p>Category: {product.category}</p>
                     <p>Rating: {product.rating}</p>
                   </div>
                 ))
-              ) : (
-                productById && (
-                <div className="product-item">
-                  <h2>{productById.title}</h2>
-                  <p>Price: {productById.price}</p>
-                  <p>Description: {productById.description}</p>
-                  <p>Category: {productById.category}</p>
-                  <p>Rating: {productById.rating}</p>
-                </div>
-                )
               )}
             </div>
           </div>
